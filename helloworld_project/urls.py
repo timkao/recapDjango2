598a8778540ced3895851e09e0aaa08696df7bfd
Tim Kao <pakuya.kao@gmail.com>
2018-10-27 17:18:08 -0400

    recap2 is done

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')) # denote where the file is pages -> urls.py
 ]
